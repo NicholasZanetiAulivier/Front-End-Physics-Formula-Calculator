3ebeef29c232595de8f03ba96fc37f05f45744b1
NicholasZanetiAulivier <auliviernicholaszaneti@gmail.com>
2025-09-08 20:49:31 +0700

    JS can read inputs

--- scripts/KPEnergy.js
+++ scripts/KPEnergy.js
@@ -1,8 +1,21 @@
 let inputForm = document.getElementById('inputForm');
-
 inputForm.addEventListener('submit', calculateResults);
+
+let inputElements = {
+    mass: document.getElementById('mass'),
+    velocity: document.getElementById('velocity'),
+    gravity: document.getElementById('gravity'),
+    height: document.getElementById('height'),
+
+    kinetic: document.getElementById('kineticEnergy'),
+    potential: document.getElementById('potentialEnergy'),
+
+    mechanical: document.getElementById('mechanicalEnergy'),
+}
+
 
 function calculateResults(e) {
     e.preventDefault();
-    alert('This is an alert');
+    alert('what');
+    console.log(inputElements);
 }
